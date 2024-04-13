3ff203cfd8a0b311b257182f17c5d96c11c5ec74
Wuaimi <141891526+Wuaimi@users.noreply.github.com>
2024-04-13 22:04:43 +0700

    updateRequest 1st commit

---
+++ frontend/src/libs/users/updateUserRequest.tsx
@@ -0,0 +1,22 @@
+export default async function updateUserRequest(
+    token: string
+  ) {
+    
+    const response = await fetch(`${process.env.BACKEND_URL}/api/users/me/campground-owner-request`, {
+      method: 'PUT',
+      headers: {
+        authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        requestToBeCampgroundOwner: true,
+      }),
+    })
+  
+    if (!response.ok) {
+      throw new Error("Cannot update user's Role")
+    }
+  
+    return await response.json()
+  }
+  
