30dac3ab7374eda10ef76904f2bc9aa4c2629034
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-04-16 15:26:17 +0700

    fix review card size

--- frontend/src/components/complex/ReviewPanel.tsx
+++ frontend/src/components/complex/ReviewPanel.tsx
@@ -6,7 +6,7 @@ export default function ReviewsPanel({
   reviews: reviewItem[]
 }) {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-9'>
+    <div className='flex flex-row'>
       {reviews.map((obj) => (
         <ReviewCard 
         userName={obj.user.name}
