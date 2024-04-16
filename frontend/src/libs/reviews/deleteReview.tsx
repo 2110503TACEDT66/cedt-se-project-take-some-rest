7f88fa20ccfd78245d5fc4c1f2196a312ce38ca5
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-04-16 16:11:27 +0700

    add deleteReview libs

---
+++ frontend/src/libs/reviews/deleteReview.tsx
@@ -0,0 +1,13 @@
+export default async function deleteReview(token: string, rvid: string) {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/reviews/${rvid}`, {
+      method: 'DELETE',
+      headers: { authorization: `Bearer ${token}` },
+    })
+  
+    if (!response.ok) {
+      throw new Error("Cannot Delete Review")
+    }
+  
+    return await response.json()
+  }
+  
