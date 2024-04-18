4816c244d106dfb4a59f371627949bab356e76cb
Piinxluna <pingpingkuss@gmail.com>
2024-04-18 23:10:35 +0700

    Edit alert for user request to be a campground owner & Edit admin users table

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -60,6 +60,8 @@ export default function ViewProfile() {
                       confirm('Are you sure to request to be campground owner?')
                     ) {
                       await updateUserRequest(session.user.token)
+                      alert('Successfully request to be a campground owner')
+                      window.location.reload()
                     }
                   }}>
                   Request
