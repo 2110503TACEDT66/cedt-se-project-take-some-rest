72ac2608802534fa8a3de19851a0848ae9852054
pppny1920 <popo192029@gmail.com>
2024-04-19 20:08:18 +0700

    review summary page

---
+++ frontend/src/app/admin/reviews/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import SuspenseUI from '@/components/basic/SuspenseUI'
+import getUsers from '@/libs/users/getUsers'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import getReviews from '@/libs/reviews/getReviews'
+
+export default function ReviewTable() {
+  const { data: session } = useSession()
+  if (!session || !session.user.token) return null
+
+  const [isReady, setIsReady] = useState(false)
+  const [review, setReview] = useState<reviewItem[]>([])
+
+  const fetchData = async () => {
+    setIsReady(false)
+    const reviewData: reviewItem[] = (
+      await  getReviews('66024afe9fd7c52c54b67f49') //edit API here (get all review)
+    ).data
+    setReview(reviewData)
+    setIsReady(true)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  //mock API for report review
+  const reportReviewMock = (review : reviewItem) => {}
+  const report = async (review : reviewItem) => {
+    await reportReviewMock(review)
+  }
+
+  if (!isReady) return <SuspenseUI />
+
+  return (
+    <main className='bg-cgr-gray-10 p-16 w-screen min-h-screen'>
+
+      <h1 className='text-cgr-black text-4xl font-bold mb-4'>Reviews</h1>
+
+      {/* Request */}
+      
+
+      {/* Normal */}
+      <div className='text-cgr-dark-green text-2xl mb-5 font-medium'>
+        User Reviews
+      </div>
+      <table className='cgr-table'>
+        <tr className='h-10'>
+          <th className='w-6/12'>Comment</th>
+          <th className='w-1/12'>Rating</th>
+          <th className='w-2/12'>Campground</th>
+          <th className='w-2/12'>User</th>
+          <th className='w-1/12'></th>
+        </tr>
+        {review.map((obj) => (
+          <tr key={obj._id}>
+            <td>{obj.comment}</td>
+            <td className='text-center'>
+              {obj.score}
+            </td>
+            <td className='text-center'>
+              {obj.campground.name}
+            </td>
+            <td className='text-center'>
+              {obj.user.name}
+            </td>
+            <td className='text-center'>
+              {obj.isReport ? (
+                <p className='text-sm'>reported</p>
+              ) : (
+                <i
+                  className='bi bi-flag-fill ml-auto mt-auto'
+                  onClick={()=>{report(obj)}}></i>
+              )}
+            </td>
+          </tr>
+        ))}
+      </table>
+    </main>
+  )
+}
