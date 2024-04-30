61ed5bd322fe7bab2e80fdf1f81de8f643ddd984
Thirathawat Chanakit <thirathawat996@gmail.com>
2024-04-30 16:53:09 +0700

    fix cypress

--- frontend/cypress/e2e/epic1/siteImage/siteImage.cy.ts
+++ frontend/cypress/e2e/epic1/siteImage/siteImage.cy.ts
@@ -1,7 +1,7 @@
 describe('Epic 1: Site Image', () => {
     beforeEach(() => {
         cy.visit(`http://localhost:3000/`);
-        cy.get('[data-testid=card]').contains('Doi Inthanon Park').click();
+        cy.get('[data-testid=card]').first().click();
         cy.get('[data-cy=viewSite]').first().click();
     });
 
